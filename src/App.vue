7e335de1bc2b34cf0edee1b02d26d06f9b678dd3
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-09-15 14:09:00 +0200

    feat: implement vue2mapbox-gl

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app>
-    chw
+    <mapbox-map />
   </v-app>
 </template>
 
 <script>
+import MapboxMap from '@/components/mapbox-map'
+
 export default {
+  components: {
+    MapboxMap
+  },
   name: 'App',
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Vue2MapboxGL from "vue2mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 Vue.config.productionTip = false
+
+Vue.use(Vue2MapboxGL);
 
 new Vue({
   vuetify,
