2c14b9edac683dcd351b248cf342336d32c7bfc6
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-09-16 12:22:16 +0200

    feat: add vuex store

---
+++ src/store/mapbox.store.js
@@ -0,0 +1,37 @@
+export default {
+  namespaced: true,
+
+  state: {
+    wmsLayers: [],
+    layerVisibilityProxies: [],
+    legendLayer: "ra2ce:erosieafschuivingwegtalud_stremmingskosten",
+  },
+
+  getters: {
+    wmsLayers: (state) => state.wmsLayers,
+    legendLayer: (state) => state.legendLayer,
+  },
+
+  mutations: {
+    ADD_WMS_LAYER(state, newLayer) {
+      const layerExists = state.wmsLayers.some(
+        (storedLayer) => storedLayer.id === newLayer.id
+      );
+
+      if (!layerExists) {
+        state.wmsLayers = [
+          ...state.wmsLayers,
+          {
+            ...newLayer,
+          },
+        ];
+      }
+    },
+    REMOVE_WMS_LAYER(state, id) {
+      state.wmsLayers = state.wmsLayers.filter((layer) => layer.id !== id);
+    },
+    SET_LEGEND_LAYER(state, layer) {
+      state.legendLayer = layer;
+    },
+  },
+};
