3ada813f773f52730b2bc256911394d8b1270fd8
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-09-16 12:22:30 +0200

    feat: add geoserver lib functions

---
+++ src/lib/wps/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { xmlRequestTemplate } from './template';
+
+export default function({ functionId, requestData, polygon, roadsIdentifier, ...rest }) {
+  const template = xmlRequestTemplate({ functionId, requestData, polygon, roadsIdentifier, ...rest });
+  const WPS_URL = 'https://webprocessingservice.openearth.nl/wps'
+  return axios({
+    method: 'post',
+    url: WPS_URL,
+    data: template,
+    headers: {'Content-Type': 'application/xml'}
+  })
+  .then(({ data }) => {
+    return typeof data === 'object'
+      ? data
+      : JSON.parse(data);
+  });
+}
