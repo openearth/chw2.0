c8ebf0b6f25d99a9ba8b9c42d835349dd1e807cb
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-09-23 17:56:49 +0200

    fix: enable welcome dialog again

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ export default new Vuex.Store({
     selection,
   },
   state: {
-    acceptedLegal: true || false,
+    acceptedLegal: false,
   },
   mutations: {
     SET_ACCEPTED_LEGAL(state, value) {
