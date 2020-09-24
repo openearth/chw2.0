8998065e0438e3b8f92a4f7af4617dc820615d0c
IoannaMi <ioannamicha@hotmail.com>
2020-09-24 15:36:13 +0200

    alter wms_layer mutation, add layerswithvisibility getter, add update_layer_visibility mutation

--- src/store/mapbox.store.js
+++ src/store/mapbox.store.js
@@ -2,7 +2,6 @@ export default {
   namespaced: true,
 
   state: {
-    layers: [],
     wmsLayers: [],
     layerVisibilityProxies: [],
     legendLayer: "ra2ce:erosieafschuivingwegtalud_stremmingskosten",
@@ -11,6 +10,14 @@ export default {
   getters: {
     wmsLayers: (state) => state.wmsLayers,
     legendLayer: (state) => state.legendLayer,
+    layersWithVisibility: state => state.wmsLayers.map(layer => {
+      const { visible } = state.layerVisibilityProxies.find(p => p.id === layer.id);
+      console.log('layer', layer)
+      return {
+        ...layer,
+        visible
+      };
+    }),
   },
 
   mutations: {
@@ -18,10 +25,20 @@ export default {
       state.layers = layers
     },
     ADD_WMS_LAYER(state, newLayer) {
-      const layerExists = state.wmsLayers.some(
-        (storedLayer) => storedLayer.id === newLayer.id
-      );
-
+      const layerExists = state.wmsLayers.some(storedLayer => storedLayer.id === newLayer.id);
+      if(!layerExists) {
+        state.wmsLayers = [
+          ...state.wmsLayers, {
+            ...newLayer
+          }
+        ];
+        state.layerVisibilityProxies = [
+          ...state.layerVisibilityProxies, {
+            id: newLayer.id,
+            visible: false
+          }
+        ];
+      }
       if (!layerExists) {
         state.wmsLayers = [
           ...state.wmsLayers,
@@ -33,6 +50,12 @@ export default {
     },
     REMOVE_WMS_LAYER(state, id) {
       state.wmsLayers = state.wmsLayers.filter((layer) => layer.id !== id);
+    },
+    UPDATE_LAYER_VISIBILITY(state, { id, map }) {
+      const layerProxy = state.layerVisibilityProxies.find(proxy => proxy.id === id);
+      const opacityToSet = layerProxy.visible ? 0 : 1;
+      map.setPaintProperty(id, 'raster-opacity', opacityToSet);
+      layerProxy.visible = !layerProxy.visible;
     },
     SET_LEGEND_LAYER(state, layer) {
       state.legendLayer = layer;
