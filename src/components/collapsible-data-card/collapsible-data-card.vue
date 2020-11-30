7a1564c7f00542f75e2abf3c234fd2f4211dcf55
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-11-30 13:06:43 +0100

    feat(collapsible-data-card): make data results collapsible

---
+++ src/components/collapsible-data-card/collapsible-data-card.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-expansion-panels v-model="open" class="mb-4">
+    <v-expansion-panel>
+      <v-expansion-panel-header class="font-weight-bold px-4">{{
+        title
+      }}</v-expansion-panel-header>
+      <v-expansion-panel-content class="mx-n6">
+        <data-table :data="data" />
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+import DataTable from '@/components/data-table';
+
+export default {
+  components: {
+    DataTable,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      open: 0,
+    };
+  },
+};
+</script>
---
+++ src/components/collapsible-data-card/index.js
@@ -0,0 +1,1 @@
+export { default } from "./collapsible-data-card";
