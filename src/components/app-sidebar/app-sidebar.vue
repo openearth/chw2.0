197f6403d26313b33cfd432b891e45e68f2cdf70
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-12-14 13:55:05 +0100

    feat(Sidebar): add permanent scrollbar to prevent layout jumping

--- src/components/app-sidebar/app-sidebar.vue
+++ src/components/app-sidebar/app-sidebar.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-navigation-drawer clipped app permanent width="400">
+  <v-navigation-drawer clipped app permanent width="400" class="hello">
     <router-view /> 
   </v-navigation-drawer>
 </template>
+
+
+<style>
+/* add this to prevent layout jumping when a scroll bar appears */
+.v-navigation-drawer__content {
+  overflow-y: scroll;
+}
+</style>
