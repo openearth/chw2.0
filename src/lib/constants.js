7d5725973c087894cb0ef31dd2c5520b86c0bc04
IoannaMi1 <ioannamicha@hotmail.com>
2022-03-19 12:45:08 +0100

    Adjust zoom and lat, long

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,15 +1,15 @@
-export const CENTER_LATITUDE =  35;
-export const CENTER_LONGITUDE = -35;
+export const CENTER_LATITUDE =  21.90;
+export const CENTER_LONGITUDE = -39.12;
 
 export const MAP_CENTER = [
   CENTER_LONGITUDE,
   CENTER_LATITUDE,
 ];
 
-export const MAP_ZOOM = 2.5
+export const MAP_ZOOM = 3
 
 export const MAP_BASELAYERS = [
-  'iomic/cl02755g2000a14qn9pmoz4j2', //TODO: personal style
+  'iomic/cl02755g2000a14qn9pmoz4j2', 
   'mapbox/satellite-v9',
 ].map(layer => ({
   style: `mapbox://styles/${ layer }`,
